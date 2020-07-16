71eded08c587fe9c9d21a5ee8c2e130dc408e5b9
liulinfan <361087115@qq.com>
2020-07-16 10:50:30 +0800

    init

---
+++ Example/Pods/Target Support Files/LFSwiftLib/LFSwiftLib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LFSwiftLib : NSObject
+@end
+@implementation PodsDummy_LFSwiftLib
+@end
---
+++ Example/Pods/Target Support Files/Pods-LFSwiftLib_Tests/Pods-LFSwiftLib_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LFSwiftLib_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_LFSwiftLib_Tests
+@end
